baf11903192e2ce855013c81567840928b7dfb44
Joy Zhou <zhou1559@umn.edu>
2023-03-06 11:32:06 -0500

    Data import and cleaning completed

--- R/week7.R
+++ R/week7.R
@@ -5,3 +5,12 @@ library(tidyverse)
 
 
 
+# Data Import and Cleaning
+week7_tbl <- read_csv(file = "../data/week3.csv") %>%
+  mutate(timeStart = as.POSIXct(timeStart, tz = "UTC")) %>%
+  mutate(condition = factor(condition, ordered = FALSE, levels = c("A", "B", "C"),labels = c("Block A", "Block B", "Control"))) %>%
+  mutate(gender = factor(gender, ordered = FALSE, levels = c("M", "F"),labels = c("Male", "Female"))) %>%
+  filter(q6 == 1) %>%
+  select(-q6) %>%
+  mutate(timeSpent = difftime(timeEnd, timeStart, units = "mins"))
+
